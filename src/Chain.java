8e4b160d82837f8b8aadf1091ec545c7816b1579
Marina30303 <mary_303@mail.ru>
2018-02-20 05:44:20 +0200

    Initial commit

---
+++ src/Chain.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by Марiна on 19.02.2018.
+ */
+public class Chain {
+
+    public static int LongestChain(String input)
+    {
+        char charArr[] = input.toCharArray();
+        int max = 0, count = 0;
+        for (int i = 0; i<input.length();i++)
+        {
+            if (charArr[i]=='1')
+            {
+                count++;
+            }else {
+                if (count > max)
+                    max = count;
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        FileInputStream in = null;
+
+        try {
+            File file = new File("input.txt");
+            in = new FileInputStream("input.txt");
+            DataOutputStream out = new DataOutputStream(new FileOutputStream("output.txt"));
+
+            byte fileData[] = new byte[(int)file.length()];
+            in.read(fileData);
+            String s = new String(fileData);
+            
+            out.writeUTF((LongestChain(s)+""));
+        }finally {
+            if (in != null) {
+                in.close();
+            }
+        }
+    }
+    }
+
